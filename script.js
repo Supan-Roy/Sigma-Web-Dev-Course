7a1e29d33d5cac40159751bdc99d19acd3ae926a
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-01-22 21:12:44 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add a confirmation prompt before opening links
-document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', event => {
-                const confirmation = confirm(`Do you want to open ${link.textContent}?`);
-                if (!confirmation) {
-                        event.preventDefault(); // Stop the link from opening
-                }
-        });
-});
